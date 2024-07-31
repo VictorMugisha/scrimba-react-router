042e3ec4121316ea2c91f4c5710cc3b91c7226f0
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-07-31 12:55:58 +0200

    Now I realized the mistake I was doing and now miragejs is working just fine

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import "./App.css"
 import Vans from './pages/Vans'
+import VanDetail from "./pages/VanDetail"
+
+import "./server"
 
 function App() {
   return (
@@ -19,6 +22,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   )
