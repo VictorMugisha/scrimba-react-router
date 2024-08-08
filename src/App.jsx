c6145fcc30731f9239b73ba1271f34399fdfc841
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-08 14:54:00 +0200

    Removed all the boilerplate code from Vans.jsx and created the login page component

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,21 @@ import {
 } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
-import Vans from './pages/vans/Vans'
+import Vans, {loader as vansLoader} from './pages/vans/Vans'
 import VanDetail from "./pages/vans/VanDetail"
 import Dashboard from './pages/hosts/Dashboard'
 import Income from './pages/hosts/Income'
 import Reviews from './pages/hosts/Reviews'
-import HostsVans from './pages/hosts/HostsVans'
+import HostVans from './pages/hosts/HostsVans'
 import HostVanDetail from './pages/hosts/HostsVanDetail'
 import HostVanInfo from './pages/hosts/HostVanInfo'
 import HostVanPricing from './pages/hosts/HostVanPricing'
 import HostVanPhotos from './pages/hosts/HostVanPhotos'
 import NotFound from './pages/NotFound'
+import Login from './pages/Login'
 import Layout from './components/Layout'
 import HostLayout from './components/HostLayout'
+import Error from './components/Error'
 
 import "./App.css"
 import "./server"
@@ -30,14 +32,23 @@ const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<Layout />}>
     <Route index element={<Home />} />
     <Route path="about" element={<About />} />
-    <Route path="vans" element={<Vans />} />
+    <Route
+      path="login"
+      element={<Login />}
+    />
+    <Route
+      path="vans"
+      element={<Vans />}
+      errorElement={<Error />}
+      loader={vansLoader}
+    />
     <Route path="vans/:id" element={<VanDetail />} />
 
-    <Route path="hosts" element={<HostLayout />}>
+    <Route path="host" element={<HostLayout />}>
       <Route index element={<Dashboard />} />
       <Route path="income" element={<Income />} />
       <Route path="reviews" element={<Reviews />} />
-      <Route path="vans" element={<HostsVans />} />
+      <Route path="vans" element={<HostVans />} />
       <Route path="vans/:id" element={<HostVanDetail />}>
         <Route index element={<HostVanInfo />} />
         <Route path="pricing" element={<HostVanPricing />} />
