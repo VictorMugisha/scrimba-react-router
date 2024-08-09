a4f2d2868ae0384c7f12c296b5a49fbc93a6d5e6
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-09 08:09:31 +0200

    Updated the src folder to match scrimba

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,16 @@ import {
 } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
-import Vans, { loader as vansLoader } from './pages/vans/Vans'
-import VanDetail, { loader as vanDetailLoader } from "./pages/vans/VanDetail"
-import Dashboard from './pages/hosts/Dashboard'
-import Income from './pages/hosts/Income'
-import Reviews from './pages/hosts/Reviews'
-import HostVans, { loader as hostVansLoader } from './pages/hosts/HostsVans'
-import HostVanDetail, { loader as hostVanDetailLoader } from './pages/hosts/HostsVanDetail'
-import HostVanInfo from './pages/hosts/HostVanInfo'
-import HostVanPricing from './pages/hosts/HostVanPricing'
-import HostVanPhotos from './pages/hosts/HostVanPhotos'
+import Vans, { loader as vansLoader } from './pages/Vans/Vans'
+import VanDetail, { loader as vanDetailLoader } from "./pages/Vans/VanDetail"
+import Dashboard from './pages/Host/Dashboard'
+import Income from './pages/Host/Income'
+import Reviews from './pages/Host/Reviews'
+import HostVans, { loader as hostVansLoader } from './pages/Host/HostsVans'
+import HostVanDetail, { loader as hostVanDetailLoader } from './pages/Host/HostsVanDetail'
+import HostVanInfo from './pages/Host/HostVanInfo'
+import HostVanPricing from './pages/Host/HostVanPricing'
+import HostVanPhotos from './pages/Host/HostVanPhotos'
 import NotFound from './pages/NotFound'
 import Login from './pages/Login'
 import Layout from './components/Layout'
